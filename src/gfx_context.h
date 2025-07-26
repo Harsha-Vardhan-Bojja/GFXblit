3eda3a7f0347536d1e40b9932cccc31ec9528a93
Harsha-Vardhan-Bojja <bbsharshavardhan@gmail.com>
2025-07-26 11:30:19 +0530

    Add's EGL context creation & basic info about the EGL and opengl
    Signed-off-by: Harsha-Vardhan-Bojja <bbsharshavardhan@gmail.com>

---
+++ src/gfx_context.h
@@ -0,0 +1,25 @@
+#ifndef GFX_CONTEXT_H
+#define GFX_CONTEXT_H
+#endif
+
+#include "gfx_blit.h"
+
+class gfx_context
+{
+private:
+
+public:
+  gfx_context();
+  ~gfx_context();
+};
+
+gfx_context::gfx_context()
+{
+}
+
+gfx_context::~gfx_context()
+{
+}
+
+
+
