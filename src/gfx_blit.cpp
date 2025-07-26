7270142ac3ecddc5b2dd46d212cb03cf5730ab62
Harsha-Vardhan-Bojja <bbsharshavardhan@gmail.com>
2025-07-26 21:48:36 +0530

    Add EGL context creation with Wayland and surfaceless fallback
    Signed-off-by: Harsha-Vardhan-Bojja <bbsharshavardhan@gmail.com>

--- src/gfx_blit.cpp
+++ src/gfx_blit.cpp
@@ -1,8 +1,35 @@
 #include "gfx_blitter.h"
+#include "gfx_context.h"
 
 extern "C" int gfx_blit(gfx_blit_image_t *src, gfx_blit_image_t *dst)
 { 
-  gfx_blitter test;
-  cout << "Executed the gfx_blit public api" << endl;
-  return 0;
+    gfx_blitter gfx_blitter_t;
+    gfx_context gfx_context_t;
+    gfx_blitter::egl_resource egl_res = {0};
+
+    int ret_status = gfx_blitter_t.load_extensions();
+    if(ret_status == gfx_blitter_t.GFX_LOAD_EXT_FAIL) {
+        cout << "[INFO]: Failed to load extensions" << endl;
+    }
+    else {
+        cout << "[INFO]: Loaded the extensions" << endl;
+    }
+
+    ret_status = gfx_context_t.get_wayaland_disp(&egl_res.wayland_display);
+    if(ret_status == gfx_context_t.WAYLAND_DSPY_FAIL) {
+        cout << "[INFO]: Failed to get wayland get display" << endl;
+    }
+    else {
+        cout  << "[INFO]: Connected to wayland display" << endl;
+    }
+
+    ret_status = gfx_context_t.gfx_egl_context_create(&egl_res);
+    if(ret_status != gfx_context_t.GFX_EGL_CONTEXT_SUCCESS) {
+        cout << "[INFO]: Failed to setup egl context creation" << endl;
+    }
+    else {
+        cout  << "[INFO]: EGL context creation success" << endl;
+    }
+    cout << "Executed the gfx_blit public api" << endl;
+    return 0;
 }
--- src/gfx_blitter.cpp
+++ src/gfx_blitter.cpp
@@ -1,5 +1,10 @@
 #include "gfx_blitter.h"
 
+PFNEGLCREATEIMAGEKHRPROC eglCreateImageKHR = nullptr;
+PFNEGLDESTROYIMAGEKHRPROC eglDestroyImageKHR = nullptr;
+PFNGLEGLIMAGETARGETTEXTURE2DOESPROC glEGLImageTargetTexture2DOES = nullptr;
+PFNEGLGETPLATFORMDISPLAYEXTPROC eglGetPlatformDisplayEXT = nullptr;
+
 gfx_blitter::gfx_blitter()
 {
     cout << "gfx_bliiter Constructer called" << endl;
@@ -8,4 +13,25 @@ gfx_blitter::gfx_blitter()
 gfx_blitter::~gfx_blitter()
 {
     cout << "gfx_bliiter Deconstructor called" << endl;
+}
+
+int gfx_blitter::load_extensions()
+{
+    eglGetPlatformDisplayEXT = (PFNEGLGETPLATFORMDISPLAYEXTPROC)
+        eglGetProcAddress("eglGetPlatformDisplayEXT");
+    eglCreateImageKHR = (PFNEGLCREATEIMAGEKHRPROC)
+        eglGetProcAddress("eglCreateImageKHR");
+    eglDestroyImageKHR = (PFNEGLDESTROYIMAGEKHRPROC)
+        eglGetProcAddress("eglDestroyImageKHR");
+    glEGLImageTargetTexture2DOES = (PFNGLEGLIMAGETARGETTEXTURE2DOESPROC)
+        eglGetProcAddress("glEGLImageTargetTexture2DOES");
+
+    if(!eglGetPlatformDisplayEXT || !eglCreateImageKHR ||
+        !eglDestroyImageKHR || !glEGLImageTargetTexture2DOES) {
+            cout << "[ERROR]: Failed to load the extensions" << endl;
+            return gfx_blitter::GFX_LOAD_EXT_FAIL;
+    }
+    else {
+        return gfx_blitter::GFX_LOAD_EXT_SUCCESS;
+    }
 }
